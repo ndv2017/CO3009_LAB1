6fde0b64f2fcd43348b01f8bc816c7aa7c3c6706
Vion <viet.nguyenducviet17@hcmut.edu.vn>
2024-12-16 02:21:38 +0700

    Ex10

--- Lab1/Core/Src/main.c
+++ Lab1/Core/Src/main.c
@@ -91,18 +91,9 @@ int main(void)
   MX_GPIO_Init();
   MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
-  HAL_GPIO_WritePin(LED_0_GPIO_Port, LED_0_Pin, GPIO_PIN_SET);
-  HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, GPIO_PIN_SET);
-  HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin, GPIO_PIN_SET);
-  HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin, GPIO_PIN_SET);
-  HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin, GPIO_PIN_SET);
-  HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_5_Pin, GPIO_PIN_SET);
-  HAL_GPIO_WritePin(LED_6_GPIO_Port, LED_6_Pin, GPIO_PIN_SET);
-  HAL_GPIO_WritePin(LED_7_GPIO_Port, LED_7_Pin, GPIO_PIN_SET);
-  HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin, GPIO_PIN_SET);
-  HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin, GPIO_PIN_SET);
-  HAL_GPIO_WritePin(LED_10_GPIO_Port, LED_10_Pin, GPIO_PIN_SET);
-  HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin, GPIO_PIN_SET);
+  int currentSecond = 0;
+  int currentMinute = 0;
+  int currentHour = 0;
   setTimer0(duration);
   /* USER CODE END 2 */
 
@@ -110,10 +101,27 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  testClock(duration);
+	  if (timer0_flag == 1) {
+		  currentSecond ++;
+		  if (currentSecond >= 60) {
+			  currentSecond = 0;
+			  currentMinute ++;
+		  }
+		  if (currentMinute >= 60) {
+			  currentMinute = 0;
+			  currentHour ++;
+		  }
+		  if (currentHour >= 12) {
+			  currentHour = 0;
+		  }
+		  setTimer0(duration);
+	  }
+	  clearAllClock();
+	  setNumberOnClock(currentSecond / 5);
+	  setNumberOnClock(currentMinute / 5);
+	  setNumberOnClock(currentHour);
 	  runTimer();
 	  HAL_Delay(1000);
-
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
